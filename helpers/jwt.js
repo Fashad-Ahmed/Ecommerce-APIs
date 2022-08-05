0cfd6062ec199de1362989098187fc3b72277421
Fashad-Ahmed <fashad.ahmed20@gmail.com>
2022-08-05 23:48:16 +0500

    helpers pushed[2]

---
+++ helpers/jwt.js
@@ -0,0 +1,28 @@
+const expressJwt = require("express-jwt");
+
+const authJwt = () => {
+  const secret = process.env.secret;
+  const api = process.env.API_URL;
+  return expressJwt({
+    secret,
+    algorithms: ["HS256"],
+    isRevoked: isRevoked,
+  }).unless({
+    path: [
+      { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
+      `${api}/users/login`,
+      `${api}/users/register`,
+    ],
+  });
+};
+
+async function isRevoked(req, payload, done) {
+  if (!payload.isAdmin) {
+    done(null, true);
+  }
+
+  done();
+}
+
+module.exports = authJwt;
