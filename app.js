2e11e94beca2f5883ac721f349f82d07e4693d8b
Fashad-Ahmed <fashad.ahmed20@gmail.com>
2022-08-05 20:04:50 +0500

    project maintainenancee commit + initialized[1]

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const PORT = process.env.PORT || 5000;
 require("dotenv/config");
 
 app.use(cors());
@@ -10,23 +11,25 @@ app.options("*", cors());
 
 //middleware
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(logger("tiny"));
+app.use(logger("dev"));
 
 //Routes
-const categoriesRoutes = require("./routes/categories");
-const productsRoutes = require("./routes/products");
-const usersRoutes = require("./routes/users");
-const ordersRoutes = require("./routes/orders");
+// const categoriesRoutes = require("./routes/categories");
+// const productsRoutes = require("./routes/products");
+// const usersRoutes = require("./routes/users");
+// const ordersRoutes = require("./routes/orders");
 
-const api = process.env.API_URL;
+// const api = process.env.API_URL;
 
-app.use(`${api}/categories`, categoriesRoutes);
-app.use(`${api}/products`, productsRoutes);
-app.use(`${api}/users`, usersRoutes);
-app.use(`${api}/orders`, ordersRoutes);
+// app.use(`${api}/categories`, categoriesRoutes);
+// app.use(`${api}/products`, productsRoutes);
+// app.use(`${api}/users`, usersRoutes);
+// app.use(`${api}/orders`, ordersRoutes);
 
 //Database
-mongoose.connect(process.env.CONNECTION_STRING, {
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     dbName: "eshop-database",
@@ -38,7 +41,28 @@ mongoose.connect(process.env.CONNECTION_STRING, {
     console.log(err);
   });
 
+//error catcher
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+//respond to client
+app.use((err, req, res, next) => {
+  const error = app.get("env") === "development" ? err : {};
+  const status = err.status || 500;
+
+  res.status(status).json({
+    message: err.message,
+    error: error,
+  });
+
+  console.log("error", err);
+  next();
+});
+
 //Server
-app.listen(3000, () => {
-  console.log("server is running http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
