ec76b64d5a609d12d831c1532ce05d960866c11e
Fashad-Ahmed <fashad.ahmed20@gmail.com>
2022-08-06 23:45:27 +0500

    order model [5]

--- models/order.js
+++ models/order.js
@@ -59,27 +59,22 @@ orderSchema.set("toJSON", {
 
 exports.Order = mongoose.model("Order", orderSchema);
 
-/**
-Order Example:
-
-{
-    "orderItems" : [
-        {
-            "quantity": 3,
-            "product" : "5fcfc406ae79b0a6a90d2585"
-        },
-        {
-            "quantity": 2,
-            "product" : "5fd293c7d3abe7295b1403c4"
-        }
-    ],
-    "shippingAddress1" : "Flowers Street , 45",
-    "shippingAddress2" : "1-B",
-    "city": "Prague",
-    "zip": "00000",
-    "country": "Czech Republic",
-    "phone": "+420702241333",
-    "user": "5fd51bc7e39ba856244a3b44"
-}
-
- */
+// {
+//     "orderItems" : [
+//         {
+//             "quantity": 3,
+//             "product" : "5fcfc406ae79b0a6a90d2585"
+//         },
+//         {
+//             "quantity": 2,
+//             "product" : "5fd293c7d3abe7295b1403c4"
+//         }
+//     ],
+//     "shippingAddress1" : "Flowers Street , 45",
+//     "shippingAddress2" : "1-B",
+//     "city": "Prague",
+//     "zip": "00000",
+//     "country": "Czech Republic",
+//     "phone": "+420702241333",
+//     "user": "5fd51bc7e39ba856244a3b44"
+// }
